b91048b688193a117512bb503eb7705780206e22
priyamathur22 <priyamathur2027@gmail.com>
2026-03-02 12:38:55 +0530

    Refactor: Migrated project from Vite to Next.js foundation

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">
+        Calorie Ai- Your Health Buddy
+      </h1>
+    </main>
+  );
+}
